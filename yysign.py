fa4aa705c87ce93852b1b12c19677aa6283a4f17
wd210010 <76995206+wd210010@users.noreply.github.com>
2024-04-26 14:47:34 +0800

    Update yysign.py

--- yysign.py
+++ yysign.py
@@ -9,13 +9,9 @@ import json,requests,os,time
 
 
 ##变量雨云账号密码 注册地址https://www.rainyun.com/NTY1NzY=_   登录后积分中心里面 赚钱积分 (如绑定微信 直接就有2000分）就可以用积分兑换主机 需要每天晚上八点蹲点
-yyusername=os.getenv("yyusername")
-yypassword=os.getenv("yypassword")
 
-
-import json ,requests ,os ,time #line:8
-yyusername =os .getenv ("yyusername")#line:12
-yypassword =os .getenv ("yypassword")#line:13
+# yyusername =os .getenv ("yyusername")#line:12
+# yypassword =os .getenv ("yypassword")#line:13
 def login_sign ():#line:17
     O00OOO00O0OO0OO00 =requests .session ()#line:18
     OOOO000000000O0O0 =O00OOO00O0OO0OO00 .post ('https://api.v2.rainyun.com/user/login',headers ={"Content-Type":"application/json"},data =json .dumps ({"field":f"{yyusername}","password":f"{yypassword}"}))#line:19
@@ -27,15 +23,9 @@ def login_sign ():#line:17
     O000O0OOOO00OOOOO ={'x-csrf-token':O000OOOOO000OOO0O ,}#line:31
     O0O0O000OOOO0OOO0 =O00OOO00O0OO0OO00 .post ('https://api.v2.rainyun.com/user/reward/tasks',headers =O000O0OOOO00OOOOO ,data =json .dumps ({"task_name":"每日签到","verifyCode":""}))#line:32
     print ('开始签到：签到结果 '+O0O0O000OOOO0OOO0 .text )#line:33
-    print ('尝试20次服务器兑换！')#line:35
-    for OO00000OO0OO0000O in range (200):#line:36
-        try:
-            OOOO00OO000O0O000 =O00OOO00O0OO0OO00 .post ('https://api.v2.rainyun.com/user/reward/items',headers =O000O0OOOO00OOOOO ,data ='{"item_id":107}')#line:37
-            OOO0O0OO0O000O0O0 =O00OOO00O0OO0OO00 .post ('https://api.v2.rainyun.com/user/reward/items',headers =O000O0OOOO00OOOOO ,data ='{"item_id":106}')#line:38
-            print (f'第{OO00000OO0OO0000O+1}次尝试兑换云服务器 '+json .loads (OOOO00OO000O0O000 .text )['message'])#line:39
-            print (f'第{OO00000OO0OO0000O+1}次尝试兑换云服务器 '+json .loads (OOO0O0OO0O000O0O0 .text )['message'])#line:40
-        except:
-            print('try later！！')
-        time .sleep (5 )#line:41
+
 if __name__ =='__main__':#line:44
-    login_sign ()#line:45
+    for i in range(len(os.getenv("yyusername").split('#'))):
+        yyusername=os.getenv("yyusername").split('#')[i]
+        yypassword=os.getenv("yypassword").split('#')[i]
+        login_sign ()#line:45
