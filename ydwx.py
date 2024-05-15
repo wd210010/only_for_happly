93cdd19bd952d538cb57f37095c2f3baf45758e3
wd210010 <76995206+wd210010@users.noreply.github.com>
2024-05-15 09:31:12 +0800

    Update ydwx.py

--- ydwx.py
+++ ydwx.py
@@ -39,7 +39,7 @@ for i in range(len(ydwx_deviceParams)):
   timestamp = str(int(round(time.time() * 1000)))
   md5 = hashlib.md5()
   md52 = hashlib.md5()
-  sig = f'action=mixc.app.memberSign.sign&apiVersion=1.0&appId=68a91a5bac6a4f3e91bf4b42856785c6&appVersion=3.32.0&deviceParams={ydwx_deviceParams[i]}&imei=2333&mallNo=20014&osVersion=12.0.1&params=eyJtYWxsTm8iOiIyMDAxNCJ9&platform=h5&timestamp={timestamp}&token={ydwx_token[i]}&P@Gkbu0shTNHjhM!7F' # 创建md5加密对象
+  sig = f'action=mixc.app.memberSign.sign&apiVersion=1.0&appId=68a91a5bac6a4f3e91bf4b42856785c6&appVersion=3.53.0&deviceParams={ydwx_deviceParams[i]}&imei=2333&mallNo=20014&osVersion=12.0.1&params=eyJtYWxsTm8iOiIyMDAxNCJ9&platform=h5&timestamp={timestamp}&token={ydwx_token[i]}&P@Gkbu0shTNHjhM!7F' # 创建md5加密对象
   md5.update(sig.encode('utf-8'))  # 指定需要加密的字符串
   sign = md5.hexdigest()  			# 加密后的字符串
   url = 'https://app.mixcapp.com/mixc/gateway'
@@ -58,7 +58,7 @@ for i in range(len(ydwx_deviceParams)):
     'Accept-Encoding': 'gzip, deflate',
     'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7'
   }
-  data =f'mallNo=20014&appId=68a91a5bac6a4f3e91bf4b42856785c6&platform=h5&imei=2333&appVersion=3.32.0&osVersion=12.0.1&action=mixc.app.memberSign.sign&apiVersion=1.0&timestamp={timestamp}&deviceParams={ydwx_deviceParams[i]}&token={ydwx_token[i]}&params=eyJtYWxsTm8iOiIyMDAxNCJ9&sign={sign}'
+  data =f'mallNo=20014&appId=68a91a5bac6a4f3e91bf4b42856785c6&platform=h5&imei=2333&appVersion=3.53.0&osVersion=12.0.1&action=mixc.app.memberSign.sign&apiVersion=1.0&timestamp={timestamp}&deviceParams={ydwx_deviceParams[i]}&token={ydwx_token[i]}&params=eyJtYWxsTm8iOiIyMDAxNCJ9&sign={sign}'
   html = requests.post(url=url,headers=headers,data=data)
   result = f'帐号{i+1}签到结果:'+'' +json.loads(html.text)['message']
   print(result)
