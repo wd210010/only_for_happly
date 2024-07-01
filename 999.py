555ffa37b26dc59f8c60487ff7d59c074e6883bb
wd210010 <76995206+wd210010@users.noreply.github.com>
2024-07-01 13:07:44 +0800

    Update 999.py

--- 999.py
+++ 999.py
@@ -7,7 +7,7 @@
 # cron "15 15 6,10,15 * * *" script-path=xxx.py,tag=匹配cron用
 # const $ = new Env('999会员中心')
 import requests
-import json
+import json,random
 import os
 from datetime import datetime
 
@@ -15,7 +15,19 @@ from datetime import datetime
 #export jjjck='807b3cc1-3473-4baa-b038-********'
 
 jjck  =os.getenv("jjjck").split('#')
+# 推送加
+plustoken = os.getenv("plustoken")
+
+#推送函数
+def Push(contents):
+    # 推送加
+    headers = {'Content-Type': 'application/json'}
+    json = {"token": plustoken, 'title': '999会员中心', 'content': contents.replace('\n', '<br>'), "template": "json"}
+    resp = requests.post(f'http://www.pushplus.plus/send', json=json, headers=headers).json()
+    print('push+推送成功' if resp['code'] == 200 else 'push+推送失败')
+
 today = datetime.now().date().strftime('%Y-%m-%d')
+
 for i in range(len(jjck)):
     Authorization = jjck[i]
     headers = {
@@ -31,54 +43,73 @@ for i in range(len(jjck)):
     try:
         resp_user = requests.get('https://mc.999.com.cn/zanmall_diy/ma/personal/user/info', headers=headers)
         phone = json.loads(resp_user.text)['data']['phone']
-    except:
-        continue
-    print(f'开始账号: {phone} 打卡')
-    checkInCodeList =[
-        {
-        "checkInCode": "mtbbs",
-        "checkInMeaning": "每天八杯水"
-    },
-        {
-        "checkInCode": "zs",
-        "checkInMeaning": "早睡"
-    },
-        {
-        "checkInCode": "ydswfz",
-        "checkInMeaning": "运动15分钟"
-    },
-        {
-        "checkInCode": "zq",
-        "checkInMeaning": "早起"
-    }
-    ]
-
-    # # 请求体（JSON）
-    for i in range(len(checkInCodeList)):
-        data = {
-            "type": "daily_health_check_in",
-            "params": {
-                "checkInCode": f"{checkInCodeList[i]['checkInCode']}",
-                "checkInTime": today
+        print(f'开始账号: {phone} 打卡')
+        checkInCodeList = [
+            {
+                "checkInCode": "mtbbs",
+                "checkInMeaning": "每天八杯水"
+            },
+            {
+                "checkInCode": "zs",
+                "checkInMeaning": "早睡"
+            },
+            {
+                "checkInCode": "ydswfz",
+                "checkInMeaning": "运动15分钟"
+            },
+            {
+                "checkInCode": "zq",
+                "checkInMeaning": "早起"
             }
-        }
-        Meaning = checkInCodeList[i]['checkInMeaning']
-        # 发送POST请求
+        ]
+
+        # # 请求体（JSON）
+        for i in range(len(checkInCodeList)):
+            data = {
+                "type": "daily_health_check_in",
+                "params": {
+                    "checkInCode": f"{checkInCodeList[i]['checkInCode']}",
+                    "checkInTime": today
+                }
+            }
+            Meaning = checkInCodeList[i]['checkInMeaning']
+            # 发送POST请求
+            try:
+                response = requests.post('https://mc.999.com.cn/zanmall_diy/ma/client/pointTaskClient/finishTask',
+                                         headers=headers, json=data)
+                result = json.loads(response.text)['data']
+                point = result['point']
+                if result['success'] == True:
+                    print(f'打卡内容{Meaning}---打卡完成 获得积分{point}')
+                else:
+                    print(f'打卡内容{Meaning}---请勿重复打卡')
+            except:
+                print('请检查抓包是否准确 个别青龙版本运行不了')
+                continue
+        #阅读文章
+        for i in range(5):
+            print('开始阅读')
+            data_read = {"type":"explore_health_knowledge","params":{"articleCode":str(random.randint(1, 20))}}
+            resp_read = requests.post('https://mc.999.com.cn/zanmall_diy/ma/client/pointTaskClient/finishTask',
+                                             headers=headers, json=data_read)
+            point=str(json.loads(resp_read.text)['data']['point'])
+            print(f'阅读成功！获得{point}积分')
+        #体检
+        data_h_test = {"type":"complete_health_testing","params":{"testCode":"202407011105550lh8tr"}}
+        resp_h_test = requests.post('https://mc.999.com.cn/zanmall_diy/ma/client/pointTaskClient/finishTask',
+                                  headers=headers, json=data_h_test)
+        point = str(json.loads(resp_h_test.text)['data']['point'])
+        print(f'体检成功！获得{point}积分')
+
         try:
-            response = requests.post('https://mc.999.com.cn/zanmall_diy/ma/client/pointTaskClient/finishTask', headers=headers, json=data)
-            result = json.loads(response.text)['data']
-            point =  result['point']
-            if result['success'] == True:
-                print(f'打卡内容{Meaning}---打卡完成 获得积分{point}')
-            else:
-                print(f'打卡内容{Meaning}---请勿重复打卡')
+            resp = requests.get('https://mc.999.com.cn/zanmall_diy/ma/personal/point/pointInfo', headers=headers)
+            totalpoints = json.loads(resp.text)['data']
+            print(f'当前拥有总积分:{totalpoints}')
         except:
-            print('请检查抓包是否准确 个别青龙版本运行不了')
             continue
-    try:
-        resp = requests.get('https://mc.999.com.cn/zanmall_diy/ma/personal/point/pointInfo', headers=headers)
-        totalpoints = json.loads(resp.text)['data']
-        print(f'当前拥有总积分:{totalpoints}')
-    except:
+    except Exception as e:
+        print(str(e))
+        msg =f'账号{i+1}可能失效！'
+        Push(contents=msg)
         continue
-    print('*'*30)
+print('*'*30)
